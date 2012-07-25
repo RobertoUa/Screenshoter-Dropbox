595cbf2ab9832333de43b51dddee793a034fec66
Volodymyr Barna <roberto13@Bigmir.net>
2012-07-25 10:24:44 +0300

    code refactoring

---
+++ src/com/roberto/main/ScreenCaptureAdapter.java
@@ -0,0 +1,73 @@
+package com.roberto.main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Callable;
+
+import javax.swing.JPanel;
+import javax.swing.event.MouseInputListener;
+
+public class ScreenCaptureAdapter extends JPanel implements KeyListener,
+		MouseInputListener, Callable<BufferedImage> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+
+	}
+
+	@Override
+	public BufferedImage call() throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+
+}
